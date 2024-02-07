08a988aa6e2326aac0709305aaeeb8035552bdac
Vladimir Kurdiukov <139125768+numawsm@users.noreply.github.com>
2024-02-07 10:50:55 +0000

    Added package json

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Smartlook Analytics",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macCatalyst(.v13)
+    ],
+    products: [
+        .library(
+            name: "SmartlookAnalytics",
+            targets: ["SmartlookAnalytics"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SmartlookAnalytics",
+            url: "https://github.com/numawsm/visualanalytics/releases/download/2.2.7.235/smartlook-ios-sdk-2.2.7.235.zip",
+            checksum: "e9adf5f7742be7d238e7359e822c2ff232f6f248df3c038acf7f6b3dc99db41e"
+        ),
+    ]
+)
